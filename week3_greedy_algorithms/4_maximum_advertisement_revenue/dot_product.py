92204f7557748ffecfff93dc6985501facd9925d
ikarofelix <ikaroisdm@gmail.com>
2024-04-05 09:33:05 -0300

    added dot_product algorithm

---
+++ week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.py
@@ -0,0 +1,22 @@
+def max_dot_product(first_sequence, second_sequence):
+    max_product = 0
+    
+    for i in range(len(first_sequence)):
+        maxFirstIndex = first_sequence.index(max(first_sequence))
+        maxSecondIndex = second_sequence.index(max(second_sequence))
+
+        max_product += first_sequence[maxFirstIndex] * second_sequence[maxSecondIndex]
+
+        del first_sequence[maxFirstIndex]
+        del second_sequence[maxSecondIndex]
+
+    return max_product
+
+
+if __name__ == '__main__':
+    n = int(input())
+    prices = list(map(int, input().split()))
+    clicks = list(map(int, input().split()))
+    assert len(prices) == len(clicks) == n
+    print(max_dot_product(prices, clicks))
+    
